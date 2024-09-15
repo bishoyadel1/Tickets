557afdb7358f5e6e58cfe486b2b04486418b0146
BISHOY <BISHOY@DESKTOP-ARVU609>
2024-09-15 16:38:01 +0300

    GenericRepository<T>     {

---
+++ Tickets.BLL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tickets.BLL.Interfaces
+{
+    public interface IGenericRepository<T>
+    {
+        public Task<T> Get(int? Id);
+        public Task<List<T>> GetAll();
+        public Task Remove(int? Id);
+
+        public Task<int> Add(T ob);
+
+        public Task Update(T ob);
+
+
+
+    }
+}
---
+++ Tickets.BLL/Repositories/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tickets.BLL.Interfaces;
+using Tickets.DLL.Context;
+
+namespace Tickets.BLL.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly TicketsDbContext context;
+
+        public GenericRepository(TicketsDbContext _context)
+        {
+            this.context = _context;
+        }
+
+        public async Task<int> Add(T ob)
+        {
+            await context.Set<T>().AddAsync(ob);
+            return context.SaveChanges();
+        }
+
+
+
+        public async Task<T> Get(int? Id)
+        => await context.Set<T>().FindAsync(Id);
+
+        public async Task<List<T>> GetAll()
+        =>await context.Set<T>().ToListAsync();
+
+        public async Task<int> Remove(T ob)
+        {
+             context.Set<T>().Remove(ob);
+            return context.SaveChanges();
+        }
+    
+
+        public async Task<int> Update(T ob)
+        {
+            context.Set<T>().Update(ob);
+            return context.SaveChanges();
+        }
+    }
+}
