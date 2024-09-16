095f98b243e24dcb5b690d27f0b736d12cb74762
BISHOY <BISHOY@DESKTOP-ARVU609>
2024-09-16 10:51:03 +0300

    NavAndFooter

--- Tickets.BLL/Interfaces/IOrganizrRepository.cs
+++ Tickets.BLL/Interfaces/IOrganizrRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tickets.BLL.Repositories;
+using Tickets.DLL.Models;
 
 namespace Tickets.BLL.Interfaces
 {
-    internal interface IOrganizrRepository
+    internal interface IOrganizrRepository : IGenericRepository<Organizer>
     {
     }
 }
--- Tickets.BLL/Repositories/OrganizrRepository.cs
+++ Tickets.BLL/Repositories/OrganizrRepository.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tickets.BLL.Interfaces;
+using Tickets.DLL.Context;
+using Tickets.DLL.Models;
 
 namespace Tickets.BLL.Repositories
 {
-    internal class Organizr
+    public class OrganizrRepository : GenericRepository<Organizer>, IOrganizrRepository
     {
+        public OrganizrRepository(TicketsDbContext _context) : base(_context)
+        {
+        }
     }
 }
