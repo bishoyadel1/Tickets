2a593491e525ee965a64dbece0723fd06a6bc915
kareem ehab <karemehab2003@gmail.com>
2024-09-23 01:21:04 +0300

    AddColumnCalledApprovedToEventTable

---
+++ Tickets/Migrations/20240922221651_AddedColumnToTheEvent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tickets.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedColumnToTheEvent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+           name: "Approved",
+           table: "Events",
+           type: "bit",
+           nullable: false, // Set to false if this column should not allow nulls
+           defaultValue: false // Specify a default value for existing rows
+       );
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+         name: "Approved",
+         table: "Event"
+     );
+        }
+    }
+}
